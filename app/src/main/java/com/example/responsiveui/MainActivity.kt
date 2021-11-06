12a28de5f554bff49792e73ac5f3b8ba716429d6
mariam-manjgaladze-1 <mariam.manjgaladze.1@btu.edu.ge>
2021-11-06 23:33:59 +0400

    owo

---
+++ app/src/main/java/com/example/responsiveui/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.responsiveui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val dunno = (findViewById<>(R.id.first3))
+    }
+}
+
+fun main(){
+
+}
