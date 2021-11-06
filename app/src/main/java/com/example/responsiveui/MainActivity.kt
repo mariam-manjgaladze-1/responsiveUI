5fdfe6e6c6a4bcbc17f64fd634033725fde55b96
mariam-manjgaladze-1 <mariam.manjgaladze.1@btu.edu.ge>
2021-11-06 23:53:56 +0400

    owo

--- app/src/main/java/com/example/responsiveui/MainActivity.kt
+++ app/src/main/java/com/example/responsiveui/MainActivity.kt
@@ -2,12 +2,17 @@ package com.example.responsiveui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val dunno = (findViewById<>(R.id.first3))
+        val firstThree = findViewById<EditText>(R.id.first3)
+        val lastThree = findViewById<EditText>(R.id.last3)
+        val letters = findViewById<EditText>(R.id.asoebi)
+        val personal_ID = findViewById<EditText>(R.id.id_Num)
+        val email_address = findViewById<EditText>(R.id.email)
     }
 }
 
